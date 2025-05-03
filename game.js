b3342ffc9737ca66b4cd091b0db59cd0cc0278ba
Pepper0x <pepperfromx@gmail.com>
2025-05-03 11:44:51 -0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,8 @@
+
+// Placeholder Tetris-style logic will be added here
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = "#fff";
+ctx.font = "20px Arial";
+ctx.fillText("Game loads here...", 50, 100);
