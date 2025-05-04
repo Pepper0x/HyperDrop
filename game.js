50945070c675f0c29f7137459ccaaa0a050ebe24
Pepper0x <pepperfromx@gmail.com>
2025-05-04 11:42:41 -0700

    Add files via upload

--- game.js
+++ game.js
@@ -77,36 +77,31 @@ function merge(matrix, player) {
   });
 }
 
+
 function rotate(matrix, dir) {
-  for (let y = 0; y < matrix.length; ++y) {
-    for (let x = 0; x < y; ++x) {
-      [matrix[x][y], matrix[y][x]] = [matrix[y][x], matrix[x][y]];
+  const transposed = matrix[0].map((_, i) => matrix.map(row => row[i]));
+  return dir > 0 ? transposed.map(row => row.reverse()) : transposed.reverse();
+}
+
+function playerRotate(dir) {
+  const original = player.matrix;
+  const rotated = rotate(player.matrix, dir);
+  player.matrix = rotated;
+
+  const pos = player.pos.x;
+  let offset = 1;
+
+  while (collide(arena, player)) {
+    player.pos.x += offset;
+    offset = -(offset + (offset > 0 ? 1 : -1));
+    if (offset > player.matrix[0].length) {
+      player.matrix = original;
+      player.pos.x = pos;
+      return;
     }
   }
-  if (dir > 0) {
-    matrix.forEach(row => row.reverse());
-  } else {
-    matrix.reverse();
-  }
 }
 
-function playerDrop() {
-  player.pos.y++;
-  if (collide(arena, player)) {
-    player.pos.y--;
-    merge(arena, player);
-    playerReset();
-    arenaSweep();
-  }
-  dropCounter = 0;
-}
-
-function playerMove(dir) {
-  player.pos.x += dir;
-  if (collide(arena, player)) {
-    player.pos.x -= dir;
-  }
-}
 
 function playerRotate(dir) {
   const pos = player.pos.x;
